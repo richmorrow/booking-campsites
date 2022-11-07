15c6a3513e17eb2e526e0d37676cd84c25d63f40
richezz <richardshilov@gmail.com>
2022-11-07 19:00:09 +0300

    Add listing firebase

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import { useState } from "react";
+import { toast } from "react-toastify";
+import Spinner from "../components/Spinner";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+import { getAuth } from "firebase/auth";
+import { v4 as uuidv4 } from "uuid";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { db } from "../firebase";
+
 
 export default function CreateListing() {
+  const auth = getAuth();
+  const [geolocationEnabled, setGeolocationEnabled] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [formData, setFormData] = useState({
-    backgroundColor: "#44014C",
-    type: "Аренда",
+    type: "rent",
     name: "",
     bedrooms: 1,
     bathrooms: 1,
@@ -15,6 +30,9 @@ export default function CreateListing() {
     offer: false,
     regularPrice: 0,
     discountedPrice: 0,
+    latitude: 0,
+    longitude: 0,
+    images: {},
   });
   const {
     type,
@@ -28,36 +46,157 @@ export default function CreateListing() {
     offer,
     regularPrice,
     discountedPrice,
+    latitude,
+    longitude,
+    images,
   } = formData;
-  function onChange() {}
+  function onChange(e) {
+    let boolean = null;
+    if (e.target.value === "true") {
+      boolean = true;
+    }
+    if (e.target.value === "false") {
+      boolean = false;
+    }
+    // Files
+    if (e.target.files) {
+      setFormData((prevState) => ({
+        ...prevState,
+        images: e.target.files,
+      }));
+    }
+    // Text/Boolean/Number
+    if (!e.target.files) {
+      setFormData((prevState) => ({
+        ...prevState,
+        [e.target.id]: boolean ?? e.target.value,
+      }));
+    }
+  }
+  async function onSubmit(e) {
+    e.preventDefault();
+    setLoading(true);
+    if (discountedPrice >= regularPrice) {
+      setLoading(false);
+      toast.error("Цена со скидкой должна быть ниже обычной цены");
+      return;
+    }
+    if (images.length > 6) {
+      setLoading(false);
+      toast.error("Возможна загрузка не более 6-ти фотографий");
+      return;
+    }
+    let geolocation = {};
+    let location;
+    if (geolocationEnabled) {
+      const response = await fetch(
+        `https://geocode-maps.yandex.ru/1.x/?apikey=d2f6ec42-c614-4988-9581-29a09ac6c048&format=json&geocode=${address}`
+      );
+      const data = await response.json();
+      console.log(data);
+      
+      // geolocation.lat = data.results[0]?.geometry.location.lat ?? 0;
+      // geolocation.lng = data.results[0]?.geometry.location.lng ?? 0;
+
+      location = data.status === "ZERO_RESULTS" && undefined;
+
+      if (location === undefined) {
+        setLoading(false);
+        toast.error("пожалуйста, укажите правильный адрес");
+        return;
+      }
+    } else {
+      geolocation.lat = latitude;
+      geolocation.lng = longitude;
+    }
+
+    async function storeImage(image) {
+      return new Promise((resolve, reject) => {
+        const storage = getStorage();
+        const filename = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`;
+        const storageRef = ref(storage, filename);
+        const uploadTask = uploadBytesResumable(storageRef, image);
+        uploadTask.on(
+          "state_changed",
+          (snapshot) => {
+            const progress =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log("Upload is " + progress + "% done");
+            switch (snapshot.state) {
+              case "paused":
+                console.log("Upload is paused");
+                break;
+              case "running":
+                console.log("Upload is running");
+                break;
+            }
+          },
+          (error) => {
+            reject(error);
+          },
+          () => {
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              resolve(downloadURL);
+            });
+          }
+        );
+      });
+    }
+
+    const imgUrls = await Promise.all(
+      [...images].map((image) => storeImage(image))
+    ).catch((error) => {
+      setLoading(false);
+      toast.error("Изображения не загружены");
+      return;
+    });
+
+    const formDataCopy = {
+      ...formData,
+      imgUrls,
+      geolocation,
+      timestamp: serverTimestamp(),
+    };
+    delete formDataCopy.images;
+    !formDataCopy.offer && delete formDataCopy.discountedPrice;
+    const docRef = await addDoc(collection(db, "listings"), formDataCopy);
+    setLoading(false);
+    toast.success("Объявление создано");
+  }
+
+  if (loading) {
+    return <Spinner />;
+  }
   return (
     <body className="w-full bg-cover bg-center bg-[url(https://images7.alphacoders.com/717/717815.jpg)]">
       <main className="max-w-md px-4 mx-auto bg-gray-100 bg-opacity-90">
         <h1 className="text-3xl text-center pt-4 font-bold">Заполните форму</h1>
-        <form>
+        <form onSubmit={onSubmit}>
           <p className="text-lg mt-6 font-semibold">Аренда / Продажа </p>
-          <div className="flex ">
+          <div className="flex">
             <button
               type="button"
               id="type"
-              value="rent"
+              value="sale"
               onClick={onChange}
-              className={`mr-3 px-7 py-3 font-medium text-sm uppercase shadow-md 
-          rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${
-            type === "Аренда" ? "bg-white text-black" : "bg-red-600 text-white"
-          }`}
+              className={`mr-3 px-7 py-3 font-medium text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${
+                type === "rent"
+                  ? "bg-white text-black"
+                  : "bg-red-600 text-white"
+              }`}
             >
               Аренда
             </button>
             <button
               type="button"
               id="type"
-              value="sell"
+              value="rent"
               onClick={onChange}
-              className={`ml-3 px-7 py-3 font-medium text-sm uppercase shadow-md 
-          rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${
-            type === "Продажа" ? "bg-white text-black" : "bg-red-600 text-white"
-          }`}
+              className={`ml-3 px-7 py-3 font-medium text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${
+                type === "sale"
+                  ? "bg-white text-black"
+                  : "bg-red-600 text-white"
+              }`}
             >
               Продажа
             </button>
@@ -170,6 +309,38 @@ export default function CreateListing() {
             className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 
           rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 mb-6"
           />
+          {!geolocationEnabled && (
+            <div className="flex space-x-6 justify-start mb-6">
+              <div className="">
+                <p className="text-lg font-semibold">Широта</p>
+                <input
+                  type="number"
+                  id="latitude"
+                  value={latitude}
+                  onChange={onChange}
+                  required
+                  min="-90"
+                  max="90"
+                  className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 
+                  ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 text-center"
+                />
+              </div>
+              <div className="">
+                <p className="text-lg font-semibold">Долгота</p>
+                <input
+                  type="number"
+                  id="longitude"
+                  value={longitude}
+                  onChange={onChange}
+                  required
+                  min="-180"
+                  max="180"
+                  className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 
+                  ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 text-center"
+                />
+              </div>
+            </div>
+          )}
           <p className="text-lg font-semibold">Описание</p>
           <textarea
             type="text"
@@ -223,7 +394,7 @@ export default function CreateListing() {
                   className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 
                 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 text-center"
                 />
-                {type === "Аренда" && (
+                {type === "rent" && (
                   <div className="">
                     <p className="text-md w-full whitespace-nowrap">
                       $ / Месяц
@@ -250,7 +421,7 @@ export default function CreateListing() {
                     className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 
                 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 text-center"
                   />
-                  {type === "Аренда" && (
+                  {type === "rent" && (
                     <div className="">
                       <p className="text-md w-full whitespace-nowrap">
                         $ / Месяц
