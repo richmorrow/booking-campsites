e4b0e3d9ae30d9514c801cdb0b9d48994bed34c2
richezz <richardshilov@gmail.com>
2022-11-13 17:01:20 +0300

    create EditListing page

---
+++ src/pages/EditListing.jsx
@@ -0,0 +1,509 @@
+import React from "react";
+import { useState } from "react";
+import { toast } from "react-toastify";
+import Spinner from "../components/Spinner";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+import { getAuth } from "firebase/auth";
+import { v4 as uuidv4 } from "uuid";
+import {
+  addDoc,
+  collection,
+  doc,
+  getDoc,
+  serverTimestamp,
+  updateDoc,
+} from "firebase/firestore";
+import { db } from "../firebase";
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect } from "react";
+
+export default function CreateListing() {
+  const navigate = useNavigate();
+  const auth = getAuth();
+  const [geolocationEnabled, setGeolocationEnabled] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [listing, setListing] = useState(null);
+  const [formData, setFormData] = useState({
+    type: "rent",
+    name: "",
+    bedrooms: 1,
+    bathrooms: 1,
+    parking: false,
+    furnished: false,
+    address: "",
+    description: "",
+    offer: false,
+    regularPrice: 0,
+    discountedPrice: 0,
+    latitude: 0,
+    longitude: 0,
+    images: {},
+  });
+  const {
+    type,
+    name,
+    bedrooms,
+    bathrooms,
+    parking,
+    furnished,
+    address,
+    description,
+    offer,
+    regularPrice,
+    discountedPrice,
+    latitude,
+    longitude,
+    images,
+  } = formData;
+
+  const params = useParams();
+
+  useEffect(() => {
+    if (listing && listing.userRef !== auth.currentUser.uid) {
+      toast.error("вы не можете редактировать это объявление");
+      navigate("/");
+    }
+  }, [auth.currentUser.uid, listing, navigate]);
+
+  useEffect(() => {
+    setLoading(true);
+    async function fetchListing() {
+      const docRef = doc(db, "listings", params.listingId);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        setListing(docSnap.data());
+        setFormData({ ...docSnap.data() });
+        setLoading(false);
+      } else {
+        navigate("/");
+        toast.error("Объявления не существует");
+      }
+    }
+    fetchListing();
+  }, [navigate, params.listingId]);
+
+ 
+
+  function onChange(e) {
+    let boolean = null;
+    if (e.target.value === "true") {
+      boolean = true;
+    }
+    if (e.target.value === "false") {
+      boolean = false;
+    }
+    // Files
+    if (e.target.files) {
+      setFormData((prevState) => ({
+        ...prevState,
+        images: e.target.files,
+      }));
+    }
+    // Text/Boolean/Number
+    if (!e.target.files) {
+      setFormData((prevState) => ({
+        ...prevState,
+        [e.target.id]: boolean ?? e.target.value,
+      }));
+    }
+  }
+  async function onSubmit(e) {
+    e.preventDefault();
+    setLoading(true);
+    if (discountedPrice >= regularPrice) {
+      setLoading(false);
+      toast.error("Цена со скидкой должна быть ниже обычной цены");
+      return;
+    }
+    if (images.length > 6) {
+      setLoading(false);
+      toast.error("Возможна загрузка не более 6-ти фотографий");
+      return;
+    }
+    let geolocation = {};
+    let location;
+    if (geolocationEnabled) {
+      const response = await fetch(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`
+        // `https://geocode-maps.yandex.ru/1.x/?apikey=d2f6ec42-c614-4988-9581-29a09ac6c048&format=json&geocode=${address}`
+      );
+      const data = await response.json();
+      console.log(data);
+
+      geolocation.lat = data.results[0]?.geometry.location.lat ?? 0;
+      geolocation.lng = data.results[0]?.geometry.location.lng ?? 0;
+
+      location = data.status === "ZERO_RESULTS" && undefined;
+
+      if (location === undefined) {
+        setLoading(false);
+        toast.error("пожалуйста, укажите правильный адрес");
+        return;
+      }
+    } else {
+      geolocation.lat = latitude;
+      geolocation.lng = longitude;
+    }
+
+    async function storeImage(image) {
+      return new Promise((resolve, reject) => {
+        const storage = getStorage();
+        const filename = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`;
+        const storageRef = ref(storage, filename);
+        const uploadTask = uploadBytesResumable(storageRef, image);
+        uploadTask.on(
+          "state_changed",
+          (snapshot) => {
+            const progress =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log("Upload is " + progress + "% done");
+            switch (snapshot.state) {
+              case "paused":
+                console.log("Upload is paused");
+                break;
+              case "running":
+                console.log("Upload is running");
+                break;
+            }
+          },
+          (error) => {
+            reject(error);
+          },
+          () => {
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              resolve(downloadURL);
+            });
+          }
+        );
+      });
+    }
+
+    const imgUrls = await Promise.all(
+      [...images].map((image) => storeImage(image))
+    ).catch((error) => {
+      setLoading(false);
+      toast.error("Изображения не загружены");
+      return;
+    });
+
+    const formDataCopy = {
+      ...formData,
+      imgUrls,
+      geolocation,
+      timestamp: serverTimestamp(),
+      userRef: auth.currentUser.uid,
+    };
+    delete formDataCopy.images;
+    !formDataCopy.offer && delete formDataCopy.discountedPrice;
+    delete formDataCopy.latitude;
+    delete formDataCopy.longitude;
+    const docRef = doc(db, "listings", params.listingId);
+    await updateDoc(docRef, formDataCopy);
+    setLoading(false);
+    toast.success("Объявление изменено");
+    navigate(`/category/${formDataCopy.type}/${docRef.id}`);
+  }
+
+  if (loading) {
+    return <Spinner />;
+  }
+  return (
+    <body className="w-full bg-cover bg-center bg-[url(https://images7.alphacoders.com/717/717815.jpg)]">
+      <main className="max-w-md px-4 mx-auto bg-gray-100 bg-opacity-90">
+        <h1 className="text-3xl text-center pt-4 font-bold">Редактирование</h1>
+        <form onSubmit={onSubmit}>
+          <p className="text-lg mt-6 font-semibold">Аренда / Продажа </p>
+          <div className="flex">
+            <button
+              type="button"
+              id="type"
+              value="sale"
+              onClick={onChange}
+              className={`mr-3 px-7 py-3 font-medium text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${
+                type === "rent"
+                  ? "bg-white text-black"
+                  : "bg-red-600 text-white"
+              }`}
+            >
+              Аренда
+            </button>
+            <button
+              type="button"
+              id="type"
+              value="rent"
+              onClick={onChange}
+              className={`ml-3 px-7 py-3 font-medium text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${
+                type === "sale"
+                  ? "bg-white text-black"
+                  : "bg-red-600 text-white"
+              }`}
+            >
+              Продажа
+            </button>
+          </div>
+          <p className="text-lg mt-6 font-semibold">Наименование</p>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={onChange}
+            placeholder="Например: Кемпинг «Рыбчино»"
+            maxLength="32"
+            minLength="10"
+            required
+            className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 
+          rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 mb-6"
+          />
+          <div className="flex space-x-6 justify-start mb-6">
+            <div>
+              <p className="text-lg font-semibold">Спальные места</p>
+              <input
+                type="number"
+                id="bedrooms"
+                value={bedrooms}
+                onChange={onChange}
+                min="1"
+                max="50"
+                required
+                className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 
+              ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 text-center"
+              />
+            </div>
+            <div>
+              <p className="text-lg font-semibold">Душевые кабины</p>
+              <input
+                type="number"
+                id="bathrooms"
+                value={bathrooms}
+                onChange={onChange}
+                min="0"
+                max="50"
+                required
+                className=" w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 
+              ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 text-center"
+              />
+            </div>
+          </div>
+          <p className="text-lg mt-6 font-semibold">Парковочные места</p>
+          <div className="flex ">
+            <button
+              type="button"
+              id="parking"
+              value={true}
+              onClick={onChange}
+              className={`mr-3 px-7 py-3 font-medium text-sm uppercase shadow-md 
+          rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${
+            !parking ? "bg-white text-black" : "bg-red-600 text-white"
+          }`}
+            >
+              Есть
+            </button>
+            <button
+              type="button"
+              id="parking"
+              value={false}
+              onClick={onChange}
+              className={`ml-3 px-7 py-3 font-medium text-sm uppercase shadow-md 
+          rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${
+            parking ? "bg-white text-black" : "bg-red-600 text-white"
+          }`}
+            >
+              Нет
+            </button>
+          </div>
+          <p className="text-lg mt-6 font-semibold">Мебель</p>
+          <div className="flex ">
+            <button
+              type="button"
+              id="furnished"
+              value={true}
+              onClick={onChange}
+              className={`mr-3 px-7 py-3 font-medium text-sm uppercase shadow-md 
+          rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${
+            !furnished ? "bg-white text-black" : "bg-red-600 text-white"
+          }`}
+            >
+              Есть
+            </button>
+            <button
+              type="button"
+              id="furnished"
+              value={false}
+              onClick={onChange}
+              className={`ml-3 px-7 py-3 font-medium text-sm uppercase shadow-md 
+          rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${
+            furnished ? "bg-white text-black" : "bg-red-600 text-white"
+          }`}
+            >
+              Нет
+            </button>
+          </div>
+          <p className="text-lg mt-6 font-semibold">Адрес</p>
+          <textarea
+            type="text"
+            id="address"
+            value={address}
+            onChange={onChange}
+            placeholder="Расположение"
+            required
+            className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 
+          rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 mb-6"
+          />
+          {!geolocationEnabled && (
+            <div className="flex space-x-6 justify-start mb-6">
+              <div className="">
+                <p className="text-lg font-semibold">Широта</p>
+                <input
+                  type="number"
+                  id="latitude"
+                  value={latitude}
+                  onChange={onChange}
+                  required
+                  min="-90"
+                  max="90"
+                  className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 
+                  ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 text-center"
+                />
+              </div>
+              <div className="">
+                <p className="text-lg font-semibold">Долгота</p>
+                <input
+                  type="number"
+                  id="longitude"
+                  value={longitude}
+                  onChange={onChange}
+                  required
+                  min="-180"
+                  max="180"
+                  className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 
+                  ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 text-center"
+                />
+              </div>
+            </div>
+          )}
+          <p className="text-lg font-semibold">Описание</p>
+          <textarea
+            type="text"
+            id="description"
+            value={description}
+            onChange={onChange}
+            placeholder="Например: Кемпинг «Рыбчино» — сказочное место для любителей активного отдыха на природе"
+            required
+            className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 
+          rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 mb-6"
+          />
+          <p className="text-lg font-semibold">Специальное предложение</p>
+          <div className="flex mb-6">
+            <button
+              type="button"
+              id="offer"
+              value={true}
+              onClick={onChange}
+              className={`mr-3 px-7 py-3 font-medium text-sm uppercase shadow-md 
+          rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${
+            !offer ? "bg-white text-black" : "bg-red-600 text-white"
+          }`}
+            >
+              Да
+            </button>
+            <button
+              type="button"
+              id="offer"
+              value={false}
+              onClick={onChange}
+              className={`ml-3 px-7 py-3 font-medium text-sm uppercase shadow-md 
+          rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${
+            offer ? "bg-white text-black" : "bg-red-600 text-white"
+          }`}
+            >
+              Нет
+            </button>
+          </div>
+          <div className="flex justify-start items-center mb-6">
+            <div>
+              <p className="text-lg font-semibold">Цена</p>
+              <div className="flex w-full justify-center items-center space-x-6">
+                <input
+                  type="number"
+                  id="regularPrice"
+                  value={regularPrice}
+                  onChange={onChange}
+                  min="50"
+                  max="400000000"
+                  required
+                  className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 
+                ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 text-center"
+                />
+                {type === "rent" && (
+                  <div className="">
+                    <p className="text-md w-full whitespace-nowrap">
+                      $ / Месяц
+                    </p>
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+
+          {offer && (
+            <div className="flex justify-start items-center mb-6">
+              <div>
+                <p className="text-lg font-semibold">Скидка</p>
+                <div className="flex w-full justify-center items-center space-x-6">
+                  <input
+                    type="number"
+                    id="discountedPrice"
+                    value={discountedPrice}
+                    onChange={onChange}
+                    min="50"
+                    max="400000000"
+                    required={offer}
+                    className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 
+                ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 text-center"
+                  />
+                  {type === "rent" && (
+                    <div className="">
+                      <p className="text-md w-full whitespace-nowrap">
+                        $ / Месяц
+                      </p>
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
+          )}
+          <div className="mb-6">
+            <p className="text-lg font-semibold">Фотографии</p>
+            <p className="text-sm text-gray-600">
+              Первое изображение будет обложкой (Кол-во: не более 6 фото)
+            </p>
+            <input
+              type="file"
+              id="images"
+              onChange={onChange}
+              accept=".jpg,.png,.jpeg"
+              multiple
+              required
+              className="w-full px-3 py-1.5 text-gray-700
+           bg-white border border-gray-300 
+            rounded transition duration-150 ease-in-out focus:bg-white focus:border-slate-600"
+            />
+          </div>
+          <button
+            type="submit"
+            className="mb-6 w-full px-7 py-3 bg-blue-700 text-white font-medium text-sm uppercase 
+        rounded shadow-md hover:bg-blue-800 hover:shadow-lg focus:bg-blue-800 focus:shadow-lg 
+        active:bg-blue-900 active:shadow-lg transition duration-150 ease-in-out"
+          >
+            Применить
+          </button>
+        </form>
+      </main>
+    </body>
+  );
+}
